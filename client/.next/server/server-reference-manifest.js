3fb491c5c5a9586933ef3fd96c2a1d275f8698ed
Vadym Zaiats <vadyk.za@gmail.com>
2023-12-11 18:39:16 +0200

    next

--- client/.next/server/server-reference-manifest.js
+++ client/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"jyM7jTuk38+WU7fJG+UWggiGb7NvufbNoF2isy/k46s=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"enP5qrlS06x6pZzB7uLnoFepWB+HP28sOvl+K6pVvSQ=\"\n}"
