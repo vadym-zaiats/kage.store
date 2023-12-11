9679b438fe2c628ea921431632de900996e9260c
Vadym Zaiats <vadyk.za@gmail.com>
2023-12-11 14:46:12 +0200

    some imgs + products json

--- client/.next/server/server-reference-manifest.js
+++ client/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"QC9x/hSe0QDZ0jzzXPCREFpdY0cerXwh+SQi9uzYNYo=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"ABDG3OHFjv4yvb6c/oP6ZZvVDz01rWNuWw4WBzzkUgk=\"\n}"
